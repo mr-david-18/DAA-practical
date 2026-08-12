e0bf003951c18eaaff9278a845195fdfaa14e366
karri david <karridevid@David Karri.>
2026-08-12 23:30:54 +0530

    Update practical 4 factorial code with iterative and recursive methods, and screenshot

--- pratical 4/factorial/factorial.py
+++ pratical 4/factorial/factorial.py
@@ -1,12 +1,50 @@
-# Time Complexity: O(n)
-# Space Complexity: O(n)
+"""
+Time Complexity:
+    - Iterative Method: O(n)
+    - Recursive Method: O(n)
 
-def factorial(n):
+Space Complexity:
+    - Iterative Method: O(1)
+    - Recursive Method: O(n)
+"""
+
+def iterative_factorial(n):
+    fact = 1
+
+    for i in range(1, n + 1):
+        fact *= i
+
+    return fact
+
+
+def recursive_factorial(n):
     if n == 0 or n == 1:
         return 1
-    return n * factorial(n - 1)
 
-n = 5
-print("Input number:", n)
-result = factorial(n)
-print(f"Factorial of {n} is:", result)
+    return n * recursive_factorial(n - 1)
+
+
+def main():
+    n = int(input("Enter a number: "))
+
+    print("\nchoose")
+    print("1. Iterative Method")
+    print("2. Recursive Method")
+
+    choice = int(input("\nEnter your choice: "))
+
+    if choice == 1:
+        ans = iterative_factorial(n)
+        print(f"\nFactorial = {ans}")
+
+    elif choice == 2:
+        ans = recursive_factorial(n)
+        print(f"\nFactorial = {ans}")
+
+    else:
+        print("Invalid Choice")
+
+
+if __name__ == "__main__":
+    main()
+
